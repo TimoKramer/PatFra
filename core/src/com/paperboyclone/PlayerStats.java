f2dd9b2b74af0fd9b87cce3510153c9b52121d30
TimoKramer <ich@tinokramer.de>
2015-05-15 13:01:01 +0200

    added basic stats functionality

---
+++ core/src/com/paperboyclone/PlayerStats.java
@@ -0,0 +1,54 @@
+package com.paperboyclone;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.sun.org.apache.xalan.internal.xsltc.dom.SimpleResultTreeImpl.SingletonIterator;
+
+public class PlayerStats {
+
+	private int score;
+	private int papers;
+	private int lives;
+	BitmapFont HUDFont;
+	
+	private static final class InstanceHolder {
+		static final PlayerStats INSTANCE = new PlayerStats(0,  10, 5);
+	}
+		
+	private PlayerStats(int score, int papers, int lives) {
+		HUDFont = new BitmapFont();
+		this.score = score;
+		this.papers = papers;
+		this.lives = lives;
+	}
+
+	public static synchronized PlayerStats getInstance() {
+		return InstanceHolder.INSTANCE;
+	}
+	
+	public void draw(Batch batch, Camera camera) {
+		HUDFont.setColor(new Color(Color.WHITE));
+		HUDFont.draw(batch, "Score: " + this.score + " Papers: " + this.papers + " Lives: " + this.lives, 
+				camera.position.x - camera.viewportWidth/2, 
+				camera.position.y + camera.viewportHeight/2);
+	}
+	
+	public void paperUp(int paperCount) {
+		this.papers += paperCount;
+	}
+	
+	public void paperDown(int paperCount) {
+		this.papers -= paperCount;
+	}
+	
+	public void scoreUp(int scoreCount) {
+		this.score += scoreCount;
+	}
+	
+	public void liveDown(int liveCount) {
+		this.lives -= liveCount;
+	}
+	
+}
---
+++ core/src/com/paperboyclone/PlayerStatsListener.java
@@ -0,0 +1,28 @@
+package com.paperboyclone;
+
+import com.paperboyclone.PlayerStats;
+
+public class PlayerStatsListener {
+	
+	PlayerStats playerStats = PlayerStats.getInstance();
+
+	public void crashWithObstacle() {
+		playerStats.liveDown(1);
+	}
+
+	public void hitSubscriberMailbox() {
+		playerStats.scoreUp(100);
+	}
+	
+	public void hitSubscriberHouse() {
+		playerStats.scoreUp(10);
+	}
+	
+	public void pickupPaperPile() {
+		playerStats.paperUp(10);
+	}
+
+	public void throwPaper() {
+		playerStats.paperDown(1);
+	}
+}
