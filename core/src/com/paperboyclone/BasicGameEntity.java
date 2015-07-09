79c60a2a634f61e64b831dd2f9b3b354308a40a9
TimoKramer <ich@timokramer.de>
2015-07-09 18:28:43 +0200

    some doc comments

--- core/src/com/paperboyclone/BasicGameEntity.java
+++ core/src/com/paperboyclone/BasicGameEntity.java
@@ -8,8 +8,12 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 /**
+ * The <code>BasicGameEntity</code>-class represents the elements of the game
+ * that one can interact with. It implements <code>IBasicGameEntity</code>-interface.
+ * All entities of the game extend <code>BasicGameEntity</code>.
+ * 
  * @author Martin Freudenberg
- *
+ * @author Timo Kramer
  */
 public class BasicGameEntity implements IBasicGameEntity{
 
@@ -17,7 +21,6 @@ public class BasicGameEntity implements IBasicGameEntity{
 	protected Vector2 position;
 	//Darstellung des Objekts
 	protected Sprite sprite;
-	
 	//Kollisions Rechteck
 	protected Rectangle boundingBox;
 	
@@ -61,12 +64,9 @@ public class BasicGameEntity implements IBasicGameEntity{
 		sprite.draw(batch);
 	}
 	
-	
 	public Sprite getSprite(){
 		return sprite;
-		
 	}
-
 	
 	public void update(float delta) {
 		
@@ -75,32 +75,41 @@ public class BasicGameEntity implements IBasicGameEntity{
 
 	public void draw(SpriteBatch batch) {
 		drawSprite(batch);
-		
 	}
-
-
 
 	@Override
 	public <T> void onCollision(IBasicGameEntity collidedObject, Class<T> Type) {
 		// TODO Auto-generated method stub
-		
 	}
 	
 	@Override
 	public <T> void notColliding(IBasicGameEntity collidedObject, Class<T> Type) {
 		// TODO Auto-generated method stub
-		
 	}
 
+	/**
+	 * Sets a bounding box for collision detection
+	 * 
+	 * @param newBoundingBox	the rectangle to use for collision detection
+	 */
 	public void setBoundingBox(Rectangle newBoundingBox){
 		boundingBox = new Rectangle(newBoundingBox);
 	}
 
+	/**
+	 * @return 	the bounding box that is used for collision detection
+	 */
 	public Rectangle getBoundingBox() {
-		
 		return new Rectangle(position.x + boundingBox.x,position.y + boundingBox.y, boundingBox.width, boundingBox.height);
 	}
 
+	/**
+	 * a method to get the class of an object.
+	 * 
+	 * @param o		the object you want to know the class of
+	 * @param clazz	the generic of the supposed class
+	 * @return		class of object if successfully converted or null if not successful
+	 */
 	public static <T> T convertInstanceOfObject(Object o, Class<T> clazz) {
 		try {
 			return clazz.cast(o);
